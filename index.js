baec3ae8ae397cf51f36234a1f9a3addb865751e
Rosie Wang <rosiedwang@yahoo.com>
2018-02-01 21:44:34 +0000

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ function getFirstSelector(selector)
 }
 
 function nestedTarget(){
-  var target= document.getElementById('nested').getElementsByClassName('target');
+  var target= document.querySelector('#nested .target');
   return target;
+}
+
+function deepestChild(){
+  var target = document.querySelector('div#grand-node div div div div');
+  return target;
+}
+
+function increaseRankBy(n)
+{
+  var target = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for(let x =0; x< target.length; x++)
+  {
+      var value = parseInt(target[x].innerHTML)+n;
+      target[x].innerHTML=value.toString();
+    
+  }
+  
 }
